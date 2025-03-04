adacb69afb20a29f03d1e144ee485a3c68d94b4e
andrevanheerden <160416636+andrevanheerden@users.noreply.github.com>
2025-03-04 19:14:47 +0200

    base finished

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,34 +1,248 @@
 import Accordion from 'react-bootstrap/Accordion';
+import Image from 'react-bootstrap/Image';
 
-function FlushExample() {
+function MovieAccordion() {
   return (
     <Accordion defaultActiveKey="0" flush>
+
       <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Header><h2>The Acolyte (2024)</h2></Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <Image src="holder.js/171x180" rounded />
+          <p>A former Padawan reunites with her Jedi Master to investigate a series of crimes but discovers the forces they confront are more sinister than they ever anticipated.</p>
         </Accordion.Body>
       </Accordion.Item>
+
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header><h2>Star Wars: The Phantom Menace (Episode I) (1999)</h2></Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <Image src="holder.js/171x180" rounded />
+          <p>Experience the heroic action and unforgettable adventures of Star Wars: Episode I - The Phantom Menace. See the first fateful steps in the journey of Anakin Skywalker.
+           Stranded on the desert planet Tatooine after rescuing young Queen Amidala from the impending invasion of Naboo, Jedi apprentice Obi-Wan Kenobi and his Jedi Master Qui-Gon
+           Jinn discover nine-year-old Anakin, who is unusually strong in the Force. Anakin wins a thrilling Podrace and with it his freedom as he leaves his home to be trained as
+           a Jedi. The heroes return to Naboo where Anakin and the Queen face massive invasion forces while the two Jedi contend with a deadly foe named Darth Maul. Only then do they
+           realize the invasion is merely the first step in a sinister scheme by the re-emergent forces of darkness known as the Sith.</p>
         </Accordion.Body>
       </Accordion.Item>
+
+      <Accordion.Item eventKey="2">
+        <Accordion.Header><h2>Star Wars: Attack of the Clones (Episode II) (2002)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Set ten years after the events of "The Phantom Menace," the Republic continues to be mired in strife and chaos. A separatist movement encompassing hundreds of planets
+             and powerful corporate alliances poses new threats to the galaxy that even the Jedi cannot stem. These moves, long planned by an as yet unrevealed and powerful force,
+             lead to the beginning of the Clone Wars -- and the beginning of the end of the Republic.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="3">
+        <Accordion.Header><h2>Star Wars: The Clone Wars (movie, 2008)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>As more star systems get swept into the Clone Wars, the valiant Jedi knights struggle to maintain order. Anakin Skywalker and his Padawan learner Ahsoka Tano embark on a
+             mission that brings them face-to-face with Jabba the Hutt. Plotting against them is evil Count Dooku and his agent, Asajj Ventress, who would ensure that the Jedis fail.
+             Meanwhile, Yoda and Obi-Wan Kenobi lead the clone army against the forces of the Dark Side.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="4">
+        <Accordion.Header><h2>Star Wars: The Clone Wars (series, 2008)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>The first weekly TV series from Lucasfilm Animation chronicles the adventures of Anakin Skywalker, Yoda, Obi-Wan Kenobi and other popular characters from the "Star Wars"
+          universe during the violent Clone Wars, as dwindling numbers of Jedi knights struggle to restore peace.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="5">
+        <Accordion.Header><h2>Star Wars: Tales of the Jedi (2022)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Six brand-new animated shorts featuring parables built around Jedi from the prequel era. Journey into the lives of two distinctly different Jedi—Ahsoka Tano and Count
+             Dooku—each will be put to the test as they make choices that will define their destinies.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="6">
+        <Accordion.Header><h2>Star Wars: Revenge of the Sith (Episode III) (2005)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>It has been three years since the Clone Wars began. Jedi Master Obi-Wan Kenobi (Ewan McGregor) and Jedi Knight Anakin Skywalker (Hayden Christensen) rescue Chancellor
+             Palpatine (Ian McDiarmid) from General Grievous, the commander of the droid armies, but Grievous escapes. Suspicions are raised within the Jedi Council concerning
+             Chancellor Palpatine, with whom Anakin has formed a bond. Asked to spy on the chancellor, and full of bitterness toward the Jedi Council, Anakin embraces the Dark Side.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="7">
+        <Accordion.Header><h2>Star Wars: Tales of the Empire (2024)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>After losing everything, young Morgan Elsbeth navigates the expanding Imperial world toward a path of vengeance; former Jedi Barriss Offee does what she must to survive
+             a rapidly changing galaxy; the choices they make will define their destinies.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="8">
+        <Accordion.Header><h2>Star Wars: The Bad Batch (2021)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Members of a unique squad of clones find their way in a changing galaxy in the aftermath of the Clone War.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="9">
+        <Accordion.Header><h2>Solo: A Star Wars Story (2018)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Young Han Solo finds adventure when he joins forces with a gang of galactic smugglers and a 190-year-old Wookie named Chewbacca. Indebted to the gangster Dryden Vos,
+             the crew devises a daring plan to travel to the mining planet Kessel to steal a batch of valuable coaxium. In need of a fast ship, Solo meets Lando Calrissian, the
+             suave owner of the perfect vessel for the dangerous mission -- the Millennium Falcon.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="10">
+        <Accordion.Header><h2>Obi-Wan Kenobi (2022)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>The Jedi Master contends with the consequences of his greatest defeat -- the downfall and corruption of his one-time friend and apprentice, Anakin Skywalker, who turned
+             to the dark side as evil Sith Lord Darth Vader.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="11">
+        <Accordion.Header><h2>Andor (2023)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Rebel spy Cassian Andor's formative years of the Rebellion.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="12">
+        <Accordion.Header><h2>Star Wars Rebels (2014)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>This animated entry in the "Star Wars" franchise fills the gap between the events of "Episode III" and "Episode IV." Imperial forces occupy a remote planet and are
+             ruling -- and ruining -- its inhabitants' lives with an iron fist. A clever, disparate crew aboard the starship Ghost takes a stand against the Empire, facing new
+             villains and colorful adversaries. The rebels on the Ghost include leader Kanan, ace pilot Hera, feisty heroine Sabine, tough guy Zeb and 14-year-old con artist Ezra.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="13">
+        <Accordion.Header><h2>Rogue One: A Star Wars Story (2016)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Former scientist Galen Erso lives on a farm with his wife and young daughter, Jyn. His peaceful existence comes crashing down when the evil Orson Krennic takes him away
+             from his beloved family. Many years later, Galen becomes the Empire's lead engineer for the most powerful weapon in the galaxy, the Death Star. Knowing that her father
+             holds the key to its destruction, Jyn joins forces with a spy and other resistance fighters to steal the space station's plans for the Rebel Alliance.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="14">
+        <Accordion.Header><h2>Star Wars: A New Hope (Episode IV) (1977)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>The Imperial Forces -- under orders from cruel Darth Vader (David Prowse) -- hold Princess Leia (Carrie Fisher) hostage, in their efforts to quell the rebellion against
+             the Galactic Empire. Luke Skywalker (Mark Hamill) and Han Solo (Harrison Ford), captain of the Millennium Falcon, work together with the companionable droid duo R2-D2
+             (Kenny Baker) and C-3PO (Anthony Daniels) to rescue the beautiful princess, help the Rebel Alliance, and restore freedom and justice to the Galaxy.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="15">
+        <Accordion.Header><h2>Star Wars: The Empire Strikes Back (Episode V) (1980)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>The adventure continues in this "Star Wars" sequel. Luke Skywalker (Mark Hamill), Han Solo (Harrison Ford), Princess Leia (Carrie Fisher) and Chewbacca (Peter Mayhew)
+             face attack by the Imperial forces and its AT-AT walkers on the ice planet Hoth. While Han and Leia escape in the Millennium Falcon, Luke travels to Dagobah in search
+             of Yoda. Only with the Jedi master's help will Luke survive when the dark side of the Force beckons him into the ultimate duel with Darth Vader (David Prowse).</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="16">
+        <Accordion.Header><h2>Star Wars: Return of the Jedi (Episode VI) (1983)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Luke Skywalker (Mark Hamill) battles horrible Jabba the Hut and cruel Darth Vader to save his comrades in the Rebel Alliance and triumph over the Galactic Empire. Han
+             Solo (Harrison Ford) and Princess Leia (Carrie Fisher) reaffirm their love and team with Chewbacca, Lando Calrissian (Billy Dee Williams), the Ewoks and the androids
+             C-3PO and R2-D2 to aid in the disruption of the Dark Side and the defeat of the evil emperor.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+      
+      <Accordion.Item eventKey="17">
+        <Accordion.Header><h2>The Mandalorian (2019)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>After the fall of the Galactic Empire, a lone gunfighter makes his way through the outer reaches of the lawless galaxy.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="18">
+        <Accordion.Header><h2>The Book of Boba Fett (2021)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>On the sands of Tatooine, bounty hunter Boba Fett and mercenary Fennec Shand navigate the Galaxy's underworld and fight for Jabba the Hutt's old territory.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="19">
+        <Accordion.Header><h2>Ahsoka (2023)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Former Jedi knight Ahsoka Tano investigates an emerging threat to a vulnerable galaxy.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="20">
+        <Accordion.Header><h2>Skeleton Crew (2024)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Skeleton Crew tells the story of four kids searching for their home planet after getting lost in the Star Wars galaxy, and the strange aliens and dangerous places they
+             encounter on their adventure.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="21">
+        <Accordion.Header><h2>Star Wars Resistance (2018)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Young New Republic pilot Kazuda Xiono is recruited by Resistance Commander Poe Dameron for a secret mission on Colossus -- an aircraft refueling station on the ocean planet of
+             Castilon. A misunderstanding causes Kaz to participate in a dangerous race to prove himself. Kaz struggles to maintain his cover as a mechanic and spy as he tries to adjust to
+             this new normal. Kaz's work as a spy takes him across the known galaxy on adventures with Poe Dameron and their droids, BB-8 and CB-23, as they seek to bring down the First
+             Order.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="22">
+        <Accordion.Header><h2>Star Wars: The Force Awakens (Episode VII) (2015)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Thirty years after the defeat of the Galactic Empire, the galaxy faces a new threat from the evil Kylo Ren (Adam Driver) and the First Order. When a defector named Finn
+             crash-lands on a desert planet, he meets Rey (Daisy Ridley), a tough scavenger whose droid contains a top-secret map. Together, the young duo joins forces with Han Solo
+             (Harrison Ford) to make sure the Resistance receives the intelligence concerning the whereabouts of Luke Skywalker (Mark Hamill), the last of the Jedi Knights.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="23">
+        <Accordion.Header><h2>Star Wars: The Last Jedi (Episode VIII) (2017)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>Luke Skywalker's peaceful and solitary existence gets upended when he encounters Rey, a young woman who shows strong signs of the Force. Her desire to learn the ways of
+             the Jedi forces Luke to make a decision that changes their lives forever. Meanwhile, Kylo Ren and General Hux lead the First Order in an all-out assault against Leia and
+             the Resistance for supremacy of the galaxy.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
+      <Accordion.Item eventKey="24">
+        <Accordion.Header><h2>Star Wars: The Rise of Skywalker (Episode IX) (2019)</h2></Accordion.Header>
+        <Accordion.Body>
+        <Image src="holder.js/171x180" rounded />
+          <p>When it's discovered that the evil Emperor Palpatine did not die at the hands of Darth Vader, the rebels must race against the clock to find out his whereabouts.
+             Finn and Poe lead the Resistance to put a stop to the First Order's plans to form a new Empire, while Rey anticipates her inevitable confrontation with Kylo Ren.
+              Warning: Some flashing-lights scenes in this film may affect photosensitive viewers.</p>
+        </Accordion.Body>
+      </Accordion.Item>
+
     </Accordion>
   );
 }
 
-export default FlushExample;
+export default MovieAccordion;
---
+++ src/components/InteractiveFormComponent.js
@@ -0,0 +1,21 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+function SubscribeForMore() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+
+      <Button variant="primary">Subscribe</Button>
+    </Form>
+  );
+}
+
+export default SubscribeForMore;
